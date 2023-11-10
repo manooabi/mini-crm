8405e19aed455faca7e4a1d48d5da948275f1380
manooabi <manoo.abhillash@gmail.com>
2023-11-10 09:21:05 +0530

    Create Table Schemas and Models

---
+++ app/Http/Controllers/Admin/DashBoardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashBoardController extends Controller
+{
+    public function index()
+    {
+       
+        return 'i am dashbaord';
+    //    return view('admin.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashBoardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::prefix('admin')->middleware('auth','isAdmin')->group(function (){
+
+Route::get('/dashboard',[DashBoardController::class,'index']);
+  
 });
